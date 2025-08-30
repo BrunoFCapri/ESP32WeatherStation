dbc7ceba99cc99bf29ecea7d160195eb8c14bb9c
Bruno Fabián Capri <89028071+BrunoFCapri@users.noreply.github.com>
2025-08-30 17:23:38 -0300

    Update boot.py
    update for API restfull

--- boot.py
+++ boot.py
@@ -1,33 +1,5 @@
-# boot.py -- run on boot-up
+# Este archivo se ejecuta al inicio (boot).
 
-try:
-  import usocket as socket
-except:
-  import socket
-
-from machine import Pin
-import network
-import time
-
-import esp
-esp.osdebug(None)
-
+# Importa el recolector de basura (garbage collector) para liberar memoria.
 import gc
 gc.collect()
-
-ssid = 'WIFIUAP-HUB'
-password = 'wifiuap1898'
-
-station = network.WLAN(network.STA_IF)
-
-station.active(True)
-station.connect(ssid, password)
-
-while station.isconnected() == False:
-  time.sleep_ms(500)
-
-print('Connection successful')
-print(station.ifconfig())
-
-led = Pin(2, Pin.OUT)
-led.on()
