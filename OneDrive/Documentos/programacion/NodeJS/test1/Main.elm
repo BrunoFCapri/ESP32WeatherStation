85d6f1698b6b8c46e1459db528962c1a5b904f67
Bruno Fabián Capri <89028071+BrunoFCapri@users.noreply.github.com>
2025-09-17 21:07:10 -0300

    Delete OneDrive/Documentos/programacion directory

--- OneDrive/Documentos/programacion/NodeJS/test1/Main.elm
+++
@@ -1,8 +0,0 @@
--- Main.elm
-module Main exposing (main)
-
-import Browser
-import Html exposing (text)
-
-main =
-    Browser.sandbox { init = (), update = \_ m -> m, view = \_ -> text "¡Hola desde Elm!" }
