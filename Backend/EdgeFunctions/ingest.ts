31c83f44a841644872f2d4c86bbe3a473ad629d4
osorio-cristian-wt <cristian.osorio@uap.edu.ar>
2025-09-24 16:35:17 -0300

    fix Update imports for Deno and Supabase

--- Backend/EdgeFunctions/ingest.ts
+++ Backend/EdgeFunctions/ingest.ts
@@ -1,5 +1,5 @@
-import { serve } from "https://deno.land/std@0.203.0/http/server.ts";
-import { createClient } from "@supabase/supabase-js";
+import { serve } from 'https://deno.land/std/http/server.ts'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.55.0'
 
 // Supabase setup
 const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
